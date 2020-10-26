1c3bb4987f3e69123e27ccc4bc4a3bc02d106ef8
nirmal <nirmal251992@gmail.com>
2020-10-26 18:07:12 +0530

    demo_google

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
 import 'login_page.dart';
 import 'sign_in.dart';
+import 'package:admob_flutter/admob_flutter.dart';
+import 'ad_manager.dart';
 
-class FirstScreen extends StatelessWidget {
+
+class FirstScreen extends StatefulWidget {
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+
+
+  AdmobReward reward;
+
+  @override
+  void initState() {
+    super.initState();
+    reward = AdmobReward(adUnitId: AdManager.rewardId);
+    reward.load();
+    reward = AdmobReward(
+        adUnitId: AdManager.rewardId,
+        listener: (event, args) {
+          if (event == AdmobAdEvent.rewarded) {
+            print("User rewarded.......");
+          }
+        });
+    reward.load();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +41,7 @@ class FirstScreen extends StatelessWidget {
       body: Center(
         child: Container(
           //padding: EdgeInsets.all(0.0),
-         // color: Colors.white,
+          // color: Colors.white,
           // decoration: BoxDecoration(
           //   gradient: LinearGradient(
           //     begin: Alignment.topRight,
@@ -22,66 +50,90 @@ class FirstScreen extends StatelessWidget {
           //   ),
           // ),
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-             // mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                // CircleAvatar(
-                //   backgroundImage: NetworkImage(
-                //     imageUrl,
-                //   ),
-                //   radius: 60,
-                //   backgroundColor: Colors.transparent,
-                // ),
-                SizedBox(height: 40),
-                Text(
-                  'NAME',
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black54),
+            mainAxisAlignment: MainAxisAlignment.start,
+            // mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+
+              RaisedButton(
+                onPressed: () async {
+                  if (await reward.isLoaded) {
+                    reward.show();
+                  }
+                },
+                child: Text("Reward Ad"),
+              ),
+              // CircleAvatar(
+              //   backgroundImage: NetworkImage(
+              //     imageUrl,
+              //   ),
+              //   radius: 60,
+              //   backgroundColor: Colors.transparent,
+              // ),
+              SizedBox(height: 40),
+              Text(
+                'NAME',
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54),
+              ),
+              Text(
+                name,
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.greenAccent,
                 ),
-                Text(
-                  name,
-                  style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.greenAccent,
-                      ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'EMAIL',
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54),
+              ),
+              Text(
+                email,
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.greenAccent,
                 ),
-                SizedBox(height: 20),
-                Text(
-                  'EMAIL',
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black54),
-                ),
-                Text(
-                  email,
-                  style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.greenAccent,
-                      ),
-                ),
-                SizedBox(height: 40),
-                RaisedButton(
-                  onPressed: () {
-                    signOutGoogle();
-                    Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) {return LoginPage();}), ModalRoute.withName('/'));
-                  },
-                  color: Colors.redAccent,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Sign Out',
-                      style: TextStyle(fontSize: 25, color: Colors.white),
-                    ),
+              ),
+              SizedBox(height: 40),
+              RaisedButton(
+                onPressed: () {
+                  signOutGoogle();
+                  Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) {return LoginPage();}), ModalRoute.withName('/'));
+                },
+                color: Colors.redAccent,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Sign Out',
+                    style: TextStyle(fontSize: 25, color: Colors.white),
                   ),
-                  elevation: 5,
+                ),
+                elevation: 5,
                 //   shape: RoundedRectangleBorder(
                 //       borderRadius: BorderRadius.circular(40)),
-                 )
-              ],
-            ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: 50,
+                    itemBuilder: (context, index) {
+                      if (index % 6 == 0 && index != 0) {
+                        return AdmobBanner(
+                            adUnitId: AdManager.bannerId,
+                            adSize: AdmobBannerSize.BANNER);
+                      }
+                      return ListTile(
+                        title: Text('Item $index'),
+                      );
+                    }
+                ),
+              ),
+            ],
+          ),
 
         ),
       ),
